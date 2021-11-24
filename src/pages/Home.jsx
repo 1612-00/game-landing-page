207f75cc1375e86413a6b5d8bde0bcd41ded6c16
1612-00 <nguyenducanh.ldb@gmail.com>
2021-11-24 14:39:43 +0700

    Completed welcome component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import SwiperCore, { EffectFade, Mousewheel, Pagination } from 'swiper';
+
+import { Welcome } from './../components/home-section';
+
+SwiperCore.use([Mousewheel, Pagination, EffectFade]);
+
+const swiperOptions = {
+    direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 0,
+    mousewheel: true,
+    pagination: true,
+    effect: 'fade',
+    speed: 1000,
+};
+
+const Home = () => {
+    return (
+        <>
+            <Swiper {...swiperOptions}>
+                <SwiperSlide>
+                    {({ isActive }) => <Welcome isActive={isActive} />}
+                </SwiperSlide>
+                <SwiperSlide>slide 2</SwiperSlide>
+            </Swiper>
+        </>
+    );
+};
+
+export default Home;
